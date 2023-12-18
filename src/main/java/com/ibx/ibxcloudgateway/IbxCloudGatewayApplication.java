4f3d8ab265d5b5b1fc1c8d80c4f98e99557d3b0a
iborraxavi <xavi.iborra.95@gmail.com>
2023-12-18 23:26:33 +0100

    Add Oauth2 authentication

--- src/main/java/com/ibx/ibxcloudgateway/IbxCloudGatewayApplication.java
+++ src/main/java/com/ibx/ibxcloudgateway/IbxCloudGatewayApplication.java
@@ -2,14 +2,15 @@ package com.ibx.ibxcloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = ReactiveUserDetailsServiceAutoConfiguration.class)
 public class IbxCloudGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IbxCloudGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IbxCloudGatewayApplication.class, args);
+    }
 
 }
